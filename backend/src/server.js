7c83bd62f1572da4ad7ba790565d60429d07f403
ShulavShakya <shakyashulav29@gmail.com>
2026-05-08 22:08:02 +0545

    Added cors for frontend connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import router from "./routes/indexRouter.js";
 import { prisma } from "./utils/prisma.js";
@@ -8,6 +9,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5051;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(express.json());
 app.use("/api", router);
