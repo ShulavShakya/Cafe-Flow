4d7de5ec57b40430d39e8224397e6edf1a04b403
saakiyeahwithc <prashantshakya404@gmail.com>
2026-05-20 19:21:47 +0545

    deployy

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,7 @@ console.log("2 - setting up app...");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: " http://192.168.254.55:5173/",
+    origin: " https://cafe-flow-kohl.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
