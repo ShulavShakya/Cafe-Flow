55c02745d3d70e2f34bae864366e8e6d9a6a1b01
saakiyeahwithc <prashantshakya404@gmail.com>
2026-05-20 20:24:31 +0545

    redeploy

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,17 +1,17 @@
-import router from "express";
-import {
-  login,
-  logout,
-  verifyMe,
-  refreshToken,
-} from "../controllers/authController.js";
-import { authenticateToken } from "../middlewares/auth.Middleware.js";
+  import router from "express";
+  import {
+    login,
+    logout,
+    verifyMe,
+    refreshToken,
+  } from "../controllers/authController.js";
+  import { authenticateToken } from "../middlewares/auth.Middleware.js";
 
-const authRouter = router.Router();
+  const authRouter = router.Router();
 
-authRouter.post("/login", login);
-authRouter.post("/logout", logout);
-authRouter.get("/me", authenticateToken, verifyMe);
-authRouter.post("/refresh", authenticateToken, refreshToken);
+  authRouter.post("/login", login);
+  authRouter.post("/logout", logout);
+  authRouter.get("/me", authenticateToken, verifyMe);
+  authRouter.post("/refresh", authenticateToken, refreshToken);
 
-export default authRouter;
+  export default authRouter;
